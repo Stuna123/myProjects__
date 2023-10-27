af29acbdfe4670c3939bb0d1f5e19876e8cedc43
francis-tabora <ftabora11@gmail.com>
2023-10-27 22:37:58 +0200

    updated index.js

--- express-api-project/index.js
+++ express-api-project/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const app = express() 
+const app = express()
 const employees = require('./Employee.js')
 const moment = require("moment")
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(express.json())
 
 // path to access our data employee and to retreive all employee
 app.get('/api/employees', (req, res) => {
@@ -15,32 +13,38 @@ app.get('/api/employees', (req, res) => {
 // retrieve single employee
 app.get('/api/employees/:name', (req, res) => {
     const checkExist = employees.some(employees => employees.name === req.params.name)
-    if(checkExist) {
-        res.json(employees.filter(employees => 
+    if (checkExist) {
+        res.json(employees.filter(employees =>
             employees.name === req.params.name))
     }
     else {
         // 400 status bad request
-        res.status(400).json({msg: `Employee ${req.params.name} doesn't exist!`})
+        res.status(400).json({ msg: `Employee ${req.params.name} doesn't exist!` })
     }
 })
 
 // create employee
 app.post('/api/employees', (req, res) => {
-    const newEmployee = {
-        name:   req.body.name,
-        email:  req.body.email,
-        age:    Math.round(Math.random() * (100 - 18) + 18),
-        added: `${moment().format()}` //moment we added
-    }
-    if(!newEmployee.name || !newEmployee.email) {
-        res.status(400).json({msg: 'Please include both a name and an email !'})
+
+    // TOUJOURS METTRE DES LOGS QUAND TU AS UN SOUCIS DANS TON CODE
+    console.log(req.body)
+
+    if (!req.body.name || !req.body.email) {
+        res.status(400).json({ msg: 'Please include both a name and an email !' })
         return
     }
-    
+
+    const newEmployee = {
+        name: req.body.name,
+        email: req.body.email,
+        age: Math.round(Math.random() * (100 - 18) + 18),
+        added: `${moment().format()}` //moment we added
+    }
+
+
     employees.push(newEmployee)
-    res.json(employees) 
-    
+    res.json(employees)
+
 })
 
 // on crée le port à partir de la variable d'environnement
