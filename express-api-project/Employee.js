1ff01eaf5ce122a5952007ce84522e5a53cb90a4
francis-tabora <ftabora11@gmail.com>
2023-10-27 18:21:45 +0200

    My project express api

---
+++ express-api-project/Employee.js
@@ -0,0 +1,22 @@
+const employees = [
+    {
+        name: "T_Francis",
+        email: "ftabora@gmail.com",
+        age: 26
+    },
+
+    {
+        name: "T_Amime",
+        email:"amime@gmail.com",
+        age: 28
+    },
+
+    {
+        name: "T_Chris",
+        email: "chris@gmail.com",
+        age: 21
+    }
+]
+
+// export our file
+module.exports = employees
---
+++ express-api-project/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express() 
+const employees = require('./Employee.js')
+const moment = require("moment")
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+// path to access our data employee and to retreive all employee
+app.get('/api/employees', (req, res) => {
+    res.json(employees)
+})
+
+// retrieve single employee
+app.get('/api/employees/:name', (req, res) => {
+    const checkExist = employees.some(employees => employees.name === req.params.name)
+    if(checkExist) {
+        res.json(employees.filter(employees => 
+            employees.name === req.params.name))
+    }
+    else {
+        // 400 status bad request
+        res.status(400).json({msg: `Employee ${req.params.name} doesn't exist!`})
+    }
+})
+
+// create employee
+app.post('/api/employees', (req, res) => {
+    const newEmployee = {
+        name:   req.body,
+        email:  req.body,
+        age:    Math.round(Math.random() * (100 - 18) + 18),
+        added: `${moment().format()}` //moment we added
+    }
+    if(!newEmployee.name || !newEmployee.email) {
+        res.status(400).json({msg: 'Please include both a name and an email !'})
+        return
+    }
+    
+    employees.push(newEmployee)
+    res.json(employees)
+    
+})
+
+// on crée le port à partir de la variable d'environnement
+const PORT = process.env.PORT || 4000 // in dev
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
